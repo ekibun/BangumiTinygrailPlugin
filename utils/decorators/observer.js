0c01f882e3bb64a6caa27c119212b87c488cc24b
ekibun <soekibun@gmail.com>
2020-07-04 02:51:15 +0800

    code cleanup

--- utils/decorators/observer.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: czy0729
- * @Date: 2019-05-11 21:57:36
- * @Last Modified by:   czy0729
- * @Last Modified time: 2019-05-11 21:57:36
- */
-import { observer } from 'mobx-react'
-
-export default observer
